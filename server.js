cc856f728ac93d3898c87addb65f866ef9d41597
LiB <lbneon@gmail.com>
2013-11-12 16:42:20 -0500

    Port and Ip update

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 var http = require('http');
 
 //Get the environment variables we need.
-var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipaddr  = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port    = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 http.createServer(function (req, res) {
 	var addr = "unknown";
