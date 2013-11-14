55abc921b4292a8779fd0aff3b56a06e4f2de688
LiB <lbneon@gmail.com>
2013-11-14 12:56:55 -0500

    update

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 #!/bin/env node
-//  OpenShift sample Node application
+//Load sample Node application
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 //Get the environment variables we need.
 var ipaddr  = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port    = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-http.createServer(function (req, res) {
+http.createServer(function (req, res) { // request and response
 	var addr = "unknown";
 	var out = "";
 	if (req.headers.hasOwnProperty('x-forwarded-for')) {
@@ -23,8 +25,21 @@ http.createServer(function (req, res) {
 		}
 	}
 	
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("Welcome to Node.js on OpenShift!\n\n");
-  res.end("Your IP address seems to be " + addr + "\n");
+	var pathname = url.parse(req.url).pathname
+	if(pathname == "/game") {
+	    fs.readFile('./ejsa/index.html', 'utf-8',function (err, data) {//read cotent
+                    if (err) throw err;
+                    res.writeHead(200, {
+                        "Content-Type": "text/html"
+                    });
+                    res.write(data);
+                    res.end();
+                });
+	} else {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write("Welcome to Node.js on OpenShift!\n\n");
+        res.end("Your IP address seems to be " + addr + "\n");
+	}
 }).listen(port, ipaddr);
+
 console.log("Server running at http://" + ipaddr + ":" + port + "/");
