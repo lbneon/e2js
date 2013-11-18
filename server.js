5e11c7d5da8c587f202bea3d91cfa30bc462df8b
LiB <lbneon@gmail.com>
2013-11-18 15:51:22 -0500

    update express

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require('express')
 var app = express.createServer();
 
 //var port    = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var port = rocess.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080); app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 // Configuration
