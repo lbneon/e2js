ad0fbc11fbe14d98c7bcc8af3b8ba6cd6f77508e
LiB <lbneon@gmail.com>
2013-11-18 16:18:47 -0500

    update

--- server.js
+++ server.js
@@ -1,73 +1,17 @@
 #!/bin/env node
 
-var express = require('express');
+var express = require('express'),
+    app = express.createServer();
 
-var App = function(){
+//Get the environment variables we need.
+var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-  // Scope
+app.use(express.logger());
 
-  var self = this;
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
 
-  // Setup
-
-  self.ipaddr  = process.env.OPENSHIFT_NODEJS_IP;
-  self.port    = parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8080;
-
-  if (typeof self.ipaddr === "undefined") {
-    console.warn('No OPENSHIFT_INTERNAL_IP environment variable');
-  };
-  
-  
-   
-
-  // Web app logic
-  ////self.routes = {};
-  ////self.routes['health'] = function(req, res){ res.send('1'); };
-////
-  ////self.routes['root'] = function(req, res){
-  ////  self.db.collection('names').find().toArray(function(err, names) {
-  ////      res.header("Content-Type:","text/json");
-  ////      res.end(JSON.stringify(names));
-  ////  });
-  ////};
-
-    // Webapp urls
-  
-  self.app  = express.createServer();
-  //self.app.get('/health', self.routes['health']);
-  //self.app.get('/', self.routes['root']);
-  self.app.get('/', function(req, res){
-    res.send(req.online.length + ' users online');
-  });
-
-  
-  //starting the nodejs server with express
-
-  self.startServer = function(){
-    self.app.listen(self.port, self.ipaddr, function(){
-      console.log('%s: Node server started on %s:%d ...', Date(Date.now()), self.ipaddr, self.port);
-    });
-  }
-
-  // Destructors
-
-  self.terminator = function(sig) {
-    if (typeof sig === "string") {
-      console.log('%s: Received %s - terminating Node server ...', Date(Date.now()), sig);
-      process.exit(1);
-    };
-    console.log('%s: Node server stopped.', Date(Date.now()) );
-  };
-
-  process.on('exit', function() { self.terminator(); });
-
-  self.terminatorSetup = function(element, index, array) {
-    process.on(element, function() { self.terminator(element); });
-  };
-
-  ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'].forEach(self.terminatorSetup);
-
-};
-
-//make a new express app
-var app = new App();
+app.listen(port);
+console.log('Express server started on port %s', app.address().port);
