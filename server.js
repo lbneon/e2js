af2646b691982b80f5e114733f7015d0b109355b
LiB <lbneon@gmail.com>
2013-11-18 16:27:56 -0500

    update

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ var express = require('express'),
 var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
 var port    = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
+app.use(express.logger());
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.send('Hello World!');
 });
 
-app.listen(port, ipaddr);  
+app.listen(port,ipaddr);
 //console.log('Express server started on port %s', app.address().port);
