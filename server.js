86371a7315291bf473665c767c19306f8b972826
LiB <lbneon@gmail.com>
2013-11-18 16:24:03 -0500

    update

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 #!/bin/env node
 
 var express = require('express'),
-    app = express.createServer();
+    app = express();
 
 //Get the environment variables we need.
-var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port    = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
+var port    = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
-app.use(express.logger());
 
 app.get('/', function(req, res){
     res.send('Hello World');
 });
 
-app.listen(port);
+app.listen(port, ipaddr);  
 //console.log('Express server started on port %s', app.address().port);
