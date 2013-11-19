7c35be8070a9acb2dbe0e029dd244f525654047d
LiB <lbneon@gmail.com>
2013-11-19 16:42:09 -0500

    render

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 #!/bin/env node
 
 var express = require('express'),
-    app = express.createServer();
+    app = express();
+//    app = express.createServer();
 
 //Get the environment variables we need.
-var ipaddr  = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
-var port    = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+var ipaddr  = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
+var port    = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 
 app.use(express.logger());
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(req, res){
     res.send('Hello World!');
+});
+
+app.get('/game', function(req, res){
+    res.render('ejsa/index.html')
+});
+
+app.get('*', function(req, res){
+    res.render('404.html', {
+        title: 'No Found'
+    })
 });
 
 app.listen(port,ipaddr);
